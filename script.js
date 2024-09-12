93827054aa0bb6cfca36b57c902dfd009aaa26a9
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2024-09-13 02:10:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	return sampleObject.key === key ? "true" : "false";
+	if(sampleObject[key]){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 // Do not change the code below
 const key = prompt("Enter Key.");
